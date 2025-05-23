1aaee5159d4066e3360c735513fdd661729cd543
Cauã Souza de Assis Candido <166147628+Cauartsz@users.noreply.github.com>
2025-05-23 00:31:21 -0300

    Delete GirtabeBot directory

--- GirtabeBot/cogs/EC_Setup.py
+++
@@ -1,9 +0,0 @@
-import discord
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-INTENTS = discord.Intents.all()
-PREFIX = ","
-OWNER_ID = int(os.getenv("OWNER_ID"))
-TOKEN = os.getenv("BOT_TOKEN")
